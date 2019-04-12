2088a6ec177ef023adf3b0e1cdf54d4f3c859a51
Amit Gupta <amithigh.gupta@gmail.com>
2019-04-12 16:15:21 +0530

    Adde FCM setup for Android, except the manifest meta config.

--- android/app/src/main/java/com/hubzu/android/MainApplication.java
+++ android/app/src/main/java/com/hubzu/android/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,7 +28,9 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
           new RNFirebasePackage(),
-            new CookieManagerPackage()
+          new RNFirebaseNotificationsPackage(),
+          new RNFirebaseMessagingPackage(),
+          new CookieManagerPackage()
       );
     }
 
